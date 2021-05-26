172fb73319cfc838215e1f88beb9e3c79baaf83c
zhangjw <48988840@qq.com>
2021-05-26 17:22:42 +0800

    【Feature】

--- sandbox/text.js
+++ sandbox/text.js
@@ -1,14 +1,20 @@
-"use strict";
-
-//
-module.exports = function bind(fn, thisArg) {
+function bind(fn, thisArg) {
   return function wrap() {
-    // arguments 是 warp的，不是 bind的，返回的方法并没有调用
-    // arguments 传入的是 用户的config配置
     var args = new Array(arguments.length);
     for (var i = 0; i < args.length; i++) {
       args[i] = arguments[i];
     }
     return fn.apply(thisArg, args);
   };
+}
+
+function a(params) {
+  console.log("我是a");
+}
+
+var obj = {
+  c1: "c1",
+  c2: "c2",
 };
+
+bind(a, obj)("123");
